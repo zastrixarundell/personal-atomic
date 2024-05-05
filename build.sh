12cc2b18428afef8646682f89d84030cb2a901eb
zastrixarundell <luka@bodroza.com>
2024-05-05 22:08:12 +0200

    Using separate repo files

--- build.sh
+++ build.sh
@@ -12,38 +12,18 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-curl -Lo /usr/bin/copr https://raw.githubusercontent.com/ublue-os/COPR-command/main/copr && \
-    chmod +x /usr/bin/copr && \
-    /usr/bin/copr enable matte-schwartz/sunshine
+#curl -Lo /usr/bin/copr https://raw.githubusercontent.com/ublue-os/COPR-command/main/copr && \
+#    chmod +x /usr/bin/copr && \
+#    /usr/bin/copr enable matte-schwartz/sunshine
 
-curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo && \
-    sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/tailscale.repo && \
+curl -Lo /etc/yum.repos.d/_copr_matte-schwartz-sunshine.repo https://copr.fedorainfracloud.org/coprs/matte-schwartz/sunshine/repo/fedora-"${RELEASE}"/matte-schwartz-sunshine-fedora-"${RELEASE}".repo && \
+	curl -Lo /etc/yum.repos.d/vscode.repo https://raw.githubusercontent.com/zastrixarundell/personal-kinoite/main/repos/vscode.repo && \
+	curl -Lo /etc/yum.repos.d/teamviewer.repo https://raw.githubusercontent.com/zastrixarundell/personal-kinoite/main/repos/teamviewer.repo && \
+	curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo && \
+	sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/tailscale.repo
 
-# Custom repos which can't be directly downloaded
-
-cat <<EOF > /etc/yum.repos.d/teamviewer.repo
-[teamviewer]
-name=TeamViewer - \$basearch
-baseurl=https://linux.teamviewer.com/yum/stable/main/binary-\$basearch/
-gpgkey=https://linux.teamviewer.com/pubkey/currentkey.asc
-gpgcheck=1
-repo_gpgcheck=1
-enabled=1
-type=rpm-md
-EOF
-
-cat <<EOF > /etc/yum.repos.d/vscode.repo
-[code]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF
-
-rpm-ostree install code corectrl goverlay ncdu podman-compose sunshine tailscale teamvier \
+rpm-ostree install code corectrl goverlay ncdu podman-compose sunshine tailscale teamviewer \
         virt-manager wireshark WoeUSB zsh
-
 
 # this installs a package from fedora repos
 #rpm-ostree install screen
