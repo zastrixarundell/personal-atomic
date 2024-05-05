1339433576c1f723dc85292635273008b98a6bed
zastrixarundell <luka@bodroza.com>
2024-05-05 21:52:24 +0200

    Initial push of build.sh

--- build.sh
+++ build.sh
@@ -12,8 +12,41 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+curl -Lo /usr/bin/copr https://raw.githubusercontent.com/ublue-os/COPR-command/main/copr && \
+    chmod +x /usr/bin/copr && \
+    /usr/bin/copr enable matte-schwartz/sunshine
+
+curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo && \
+    sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/tailscale.repo && \
+
+# Custom repos which can't be directly downloaded
+
+cat <<EOF > /etc/yum.repos.d/teamviewer.repo
+[teamviewer]
+name=TeamViewer - \$basearch
+baseurl=https://linux.teamviewer.com/yum/stable/main/binary-\$basearch/
+gpgkey=https://linux.teamviewer.com/pubkey/currentkey.asc
+gpgcheck=1
+repo_gpgcheck=1
+enabled=1
+type=rpm-md
+EOF
+
+cat <<EOF | /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+rpm-ostree install code corectrl goverlay ncdu podman-compose sunshine tailscale teamvier \
+        virt-manager wireshark WoeUSB zsh
+
+
 # this installs a package from fedora repos
-rpm-ostree install screen
+#rpm-ostree install screen
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
@@ -21,3 +54,7 @@ rpm-ostree install screen
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+ln -s /usr/lib/opt/teamviewer/tv_bin/script/teamviewerd.service /etc/systemd/system/teamviewerd.service
+
+systemctl enable teamviewerd.service
