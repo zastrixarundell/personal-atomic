59535c59035286ef47301e0f8a358ee9c748411e
zastrixarundell <luka@bodroza.com>
2024-05-05 22:11:26 +0200

    Remove firefox
    Librewolf will be used in a flatpak either way

--- build.sh
+++ build.sh
@@ -25,6 +25,8 @@ curl -Lo /etc/yum.repos.d/_copr_matte-schwartz-sunshine.repo https://copr.fedora
 rpm-ostree install code corectrl goverlay ncdu podman-compose sunshine tailscale teamviewer \
         virt-manager wireshark WoeUSB zsh
 
+rpm-ostree uninstall firefox firefox-langpacks
+
 # this installs a package from fedora repos
 #rpm-ostree install screen
 
