6e3d6bdb1accd7e744e2554c64fdac8ea4ad6c39
zastrixarundell <luka@bodroza.com>
2024-06-08 18:03:33 +0200

    Only use mesa_git when it's set

--- build.sh
+++ build.sh
@@ -24,12 +24,14 @@ rpm-ostree install code corectrl goverlay ncdu podman-compose sunshine tailscale
 
 rpm-ostree uninstall firefox firefox-langpacks
 
-rpm-ostree override remove mesa-va-drivers-freeworld
+if [ -n "$MESA_GIT" ]; then
+	rpm-ostree override remove mesa-va-drivers-freeworld
 
-rpm-ostree override --experimental replace mesa-libglapi mesa-libxatracker mesa-dri-drivers mesa-libgbm mesa-libEGL mesa-libGL \
-        mesa-filesystem mesa-vdpau-drivers mesa-vulkan-drivers --from repo=mesa-git;
+	rpm-ostree override --experimental replace mesa-libglapi mesa-libxatracker mesa-dri-drivers mesa-libgbm mesa-libEGL mesa-libGL \
+			mesa-filesystem mesa-vdpau-drivers mesa-vulkan-drivers --from repo=mesa-git;
 
-rpm-ostree install mesa-va-drivers;
+	rpm-ostree install mesa-va-drivers;
+fi
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
