96a5b6844210ec7cc923878414216425909b6761
zastrixarundell <luka@bodroza.com>
2024-06-25 13:05:02 +0200

    Add forgotten backslashes

--- build.sh
+++ build.sh
@@ -18,10 +18,10 @@ RELEASE="$(rpm -E %fedora)"
 
 rpm-ostree override remove mesa-va-drivers-freeworld
 
-rpm-ostree override --experimental replace
-	mesa-libglapi mesa-libxatracker mesa-dri-drivers
-	mesa-libgbm mesa-libEGL mesa-libGL
-	mesa-filesystem mesa-vdpau-drivers mesa-vulkan-drivers
+rpm-ostree override --experimental replace \
+	mesa-libglapi mesa-libxatracker mesa-dri-drivers \
+	mesa-libgbm mesa-libEGL mesa-libGL \
+	mesa-filesystem mesa-vdpau-drivers mesa-vulkan-drivers \
 	--from repo=mesa-git
 
 rpm-ostree install mesa-va-drivers
