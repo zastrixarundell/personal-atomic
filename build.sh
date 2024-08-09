725bd6af3a7e53af01f4aa3921b648b888cf40e2
Luka Bodroža <luka@bodroza.com>
2024-08-09 14:41:54 +0200

    Add android-tools

--- build.sh
+++ build.sh
@@ -32,7 +32,22 @@ curl -Lo /etc/yum.repos.d/_copr_matte-schwartz-sunshine.repo https://copr.fedora
 
 # virt-manager
 
-rpm-ostree install code corectrl goverlay ncdu podman-compose sunshine tailscale wireshark WoeUSB zsh fastfetch krdp krdc realtime-setup
+rpm-ostree install \
+	code \
+	corectrl \
+ 	goverlay \
+  	ncdu \
+   	podman-compose \
+    	sunshine \
+     	tailscale \
+      	wireshark\
+       	WoeUSB \
+	zsh \
+ 	fastfetch \
+  	krdp \
+   	krdc \
+    	realtime-setup \
+     	android-tools
 
 rpm-ostree uninstall firefox firefox-langpacks
 
