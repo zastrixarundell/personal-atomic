4fdaca18bc352f9622052d0cf7e98bab2034d691
zastrixarundell <luka@bodroza.com>
2024-10-09 01:30:28 +0200

    Potential support for corectrl no password and btop

--- build.sh
+++ build.sh
@@ -32,7 +32,8 @@ rpm-ostree install \
     fastfetch \
     realtime-setup \
     android-tools \
-    qpwgraph
+    qpwgraph \
+    btop
 
 rpm-ostree uninstall firefox firefox-langpacks
 
@@ -69,3 +70,7 @@ cp /usr/share/pipewire/pipewire.conf /etc/pipewire/
 
 sed -i 's/#default.clock.min-quantum   = 32/default.clock.min-quantum    = 1024/g'   /etc/pipewire/pipewire.conf
 sed -i 's/#default.clock.max-quantum   = 2048/default.clock.max-quantum    = 1024/g' /etc/pipewire/pipewire.conf
+
+# Corectrl without password
+
+groupadd corectrl
