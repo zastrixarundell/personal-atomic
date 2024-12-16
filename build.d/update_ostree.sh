56776754f7655b3615adb7e5592680c03d06346e
zastrixarundell <luka@bodroza.com>
2024-12-16 11:41:14 +0100

    Update ffmpeg

--- build.d/update_ostree.sh
+++ build.d/update_ostree.sh
@@ -3,5 +3,9 @@
 INSTALL=$(cat /tmp/packages.d/install.json | jq -r '[.[]] | join(" ")')
 UNINSTALL=$(cat /tmp/packages.d/uninstall.json | jq -r '[.[]] | join(" ")')
 
+rpm-ostree install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 rpm-ostree install $INSTALL
 rpm-ostree uninstall $UNINSTALL
