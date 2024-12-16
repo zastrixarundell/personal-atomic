3aac6a532817f1cf8c72a28e795cdfec29c43f61
zastrixarundell <luka@bodroza.com>
2024-12-16 11:46:41 +0100

    Use $RELEASE

--- build.d/update_ostree.sh
+++ build.d/update_ostree.sh
@@ -8,8 +8,8 @@ INSTALL=$(cat /tmp/packages.d/install.json | jq -r '[.[]] | join(" ")')
 UNINSTALL=$(cat /tmp/packages.d/uninstall.json | jq -r '[.[]] | join(" ")')
 
 rpm-ostree install \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
 
 rpm-ostree uninstall $UNINSTALL
 rpm-ostree install $INSTALL
