1da9fe09e782a1fba382fe0b2868f80aa7f5a5b1
zastrixarundell <luka@bodroza.com>
2025-01-03 04:10:57 +0100

    Remove old curl for copr

--- build.d/sources.sh
+++ build.d/sources.sh
@@ -4,8 +4,6 @@ dnf copr enable -y kylegospo/bees &
 
 dnf copr enable -y matte-schwartz/sunshine &
 
-curl -Lo /etc/yum.repos.d/_copr_kylegospo_bees.repo https://copr.fedorainfracloud.org/coprs/kylegospo/bees/repo/fedora-${RELEASE}/kylegospo-bees-fedora-${RELEASE}.repo &
-
 (
     curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo &&
     sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/tailscale.repo
