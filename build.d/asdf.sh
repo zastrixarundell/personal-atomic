82e4f2b54912192a5231caa3cc54624cc683337e
zastrixarundell <luka@bodroza.com>
2025-05-27 10:38:13 +0200

    Make all logs silent

--- build.d/asdf.sh
+++ build.d/asdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-download_url=$(curl --silent https://api.github.com/repos/asdf-vm/asdf/releases/latest | jq --raw-output '.assets | map(select(.name | endswith("-linux-amd64.tar.gz"))) | first | .browser_download_url')
+download_url=$(curl -s https://api.github.com/repos/asdf-vm/asdf/releases/latest | jq --raw-output '.assets | map(select(.name | endswith("-linux-amd64.tar.gz"))) | first | .browser_download_url')
 wget $download_url -O /tmp/asdf-package.tar.gz
 tar -xzvf /tmp/asdf-package.tar.gz -C /usr/bin asdf
 
--- build.d/sources.sh
+++ build.d/sources.sh
@@ -3,16 +3,16 @@
 dnf copr enable -y lizardbyte/beta &
 
 (
-    curl -Lo /etc/yum.repos.d/cloudflare-warp.repo https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo
+    curl -Los /etc/yum.repos.d/cloudflare-warp.repo https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo
 ) &
 
 (
-    curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo &&
+    curl -Los /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo &&
     sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/tailscale.repo
 ) &
 
 (
-    rustdesk_url=$(curl --silent https://api.github.com/repos/rustdesk/rustdesk/releases/latest | jq --raw-output '.assets | map(select(.name | endswith("x86_64.rpm"))) | first | .browser_download_url') &&
+    rustdesk_url=$(curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest | jq --raw-output '.assets | map(select(.name | endswith("x86_64.rpm"))) | first | .browser_download_url') &&
     wget $rustdesk_url -O /tmp/rustdesk.rpm
 ) &
 
