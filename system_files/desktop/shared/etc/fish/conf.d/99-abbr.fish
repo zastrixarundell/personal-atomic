8c08c0d6cc00c4ff59f81c52a917cc3a633d463a
zastrixarundell <luka@bodroza.com>
2025-06-22 16:47:44 +0200

    Add fish abbr to /etc

---
+++ system_files/desktop/shared/etc/fish/conf.d/99-abbr.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Git stuff
+    abbr -a gc "git commit"
+    abbr -a gca "git add . && git commit -a"
+    abbr -a amend "git commit --amend"
+
+    abbr -a gcm --set-cursor=! git commit -m \"!\"
+    abbr -a gcam --set-cursor=! "git add . && git commit -a -m \"!\""
+    abbr -a gsp --set-cursor=! git stash push -m \"!\"
+end
