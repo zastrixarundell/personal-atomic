1108a74eacafebdf53c1a9ec00d39310a2e0522f
zastrixarundell <luka@bodroza.com>
2025-11-18 10:19:26 +0100

    Remove trailing s on end

--- system_files/shared/etc/fish/conf.d/96-elixir.fish
+++ system_files/shared/etc/fish/conf.d/96-elixir.fish
@@ -2,4 +2,5 @@ if status is-interactive
     abbr -a hexsrch mix hex.search --package
     abbr -a hexdocs mix hex.docs online
     abbr -a setup_phoenix mix hex.archive install phx_new
-ends
+end
+
